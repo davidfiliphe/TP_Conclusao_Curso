de2c8fb1eee35f64ee40047f9241425dead4dc69
viniciusmarquessouza <vmarques2372@gmail.com>
2024-06-27 20:18:14 -0300

    relatorio
    relatorio

---
+++ Model/RelatorioPagamento.php
@@ -0,0 +1,21 @@
+
+
+// Exemplo de variável com dash
+$nome_completo = "João da Silva";
+
+// Exemplo de função com dash
+function calcular-idade($ano_nascimento) {
+    $idade = date("Y") - $ano_nascimento;
+    return $idade;
+}
+
+// Exemplo de array com chave contendo dash
+$dados_pessoa = array(
+    "nome-completo" => "Maria Oliveira",
+    "idade" => 30,
+    "cidade-natal" => "São Paulo"
+);
+
+// Exemplo de uso de dash em URL
+$url = "https://www.exemplo.com/pagina-com-dash";
+
